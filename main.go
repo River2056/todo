1e8edf625c622a42cc2b2727ed56a51f5f709d6c
Kevin_Tung__ <kevin.tung@hytechtech.com>
2023-10-25 11:40:43 +0800

    modify

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "math/rand"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"strings"
 	"todo/assets"
@@ -13,41 +13,41 @@ import (
 )
 
 func main() {
-    todoList := make([]models.TodoItem, 0)
+	todoList := make([]models.TodoItem, 0)
 
-    allHtmls := assets.GetAllTemplates()
+	allHtmls := assets.GetAllTemplates()
 	router := gin.Default()
 	router.LoadHTMLFiles(allHtmls...)
 
 	router.GET("/", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.html", gin.H{})
+		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 
-    router.GET("/fetch-todos", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "todolist.html", gin.H{
-            "todoList": todoList,
-        })
-    })
+	router.GET("/fetch-todos", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "todolist.html", gin.H{
+			"todoList": todoList,
+		})
+	})
 
-    router.POST("/add", func(c *gin.Context) {
-        jsonBytes, err := ioutil.ReadAll(c.Request.Body)
-        if err != nil {
-            log.Fatalf("error while reading post body, %v\n", err)
-        }
-        jsonData := string(jsonBytes)
-        jsonData = strings.Replace(jsonData, "%20", " ", -1)
-        jsonArr := strings.Split(jsonData, "=")
-        t := models.TodoItem{
-            Id: rand.Int(),
-            Content: jsonArr[1],
-            IsDone: false,
-        }
-        todoList = append([]models.TodoItem{t}, todoList...)
+	router.POST("/add", func(c *gin.Context) {
+		jsonBytes, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			log.Fatalf("error while reading post body, %v\n", err)
+		}
+		jsonData := string(jsonBytes)
+		jsonData = strings.Replace(jsonData, "%20", " ", -1)
+		jsonArr := strings.Split(jsonData, "=")
+		t := models.TodoItem{
+			Id:      rand.Int(),
+			Content: jsonArr[1],
+			IsDone:  false,
+		}
+		todoList = append([]models.TodoItem{t}, todoList...)
 
-        c.HTML(http.StatusOK, "todolist.html", gin.H{
-            "todoList": todoList,
-        })
-    })
+		c.HTML(http.StatusOK, "todolist.html", gin.H{
+			"todoList": todoList,
+		})
+	})
 
-	router.Run(":8080")
+	router.Run(":9000")
 }
